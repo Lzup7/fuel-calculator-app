e6b7dae00568be2c81951c60f236295b2ff2dd0e
Lucas Olvr. <lucasgamerzup@gmail.com>
2024-09-11 16:34:27 -0300

    add
    add project

---
+++ styles.js
@@ -0,0 +1,29 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    padding: 20,
+    backgroundColor: "#fff",
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: "bold",
+    marginBottom: 20,
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: "#ccc",
+    padding: 10,
+    marginBottom: 20,
+    borderRadius: 5,
+  },
+  result: {
+    marginTop: 20,
+    fontSize: 18,
+    color: "green",
+  },
+});
+
+export default styles;
